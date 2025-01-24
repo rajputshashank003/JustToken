a79a72d95664d7e0448a420685d4de7f7e94fbea
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2025-01-24 18:54:11 +0530

    vercel bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import commonjs from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(),],
+  plugins: [react(), nodePolyfills(), commonjs()],
+  build : {
+    target: 'esnext',
+  }
 })
 
